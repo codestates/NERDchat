a08ead1963a28668ca2805f5c37a21961b97263b
Mangpha <78544031+Mangpha@users.noreply.github.com>
2021-09-09 01:39:09 +0900

    [SR] Fix: Test Code (#120)
    * [SR] Setting Test Code
    
    * Server dev (#1)
    
    * [SR] Setting Test Code
    
    * [SR] Setting Test Code (#116)
    
    * [SR] Fix: Test Code
    
    * [SR] Fix: Test Code
    
    * [SR] Fix: Test Code

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 sudo chmod -R 777 /home/ubuntu/server
 cd /home/ubuntu/server
-sudo pm2 stop index
-sudo pm2 delete index
+sudo pm2 stop index 2> /dev/null || true
+sudo pm2 delete index 2> /dev/null || true
