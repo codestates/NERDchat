43f77a2af036731755733c57d56100bb01dc133a
Mangpha <8350130@gmail.co>
2021-09-09 03:22:45 +0900

    Server AUto

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/bin/bash
+sudo chmod -R 777 /home/ubuntu/server
 cd /home/ubuntu/server
-pm2 stop index 2> /dev/null || true
-pm2 delete index 2> /dev/null || true
+npm i pm2 -g
+sudo pm2 delete index
