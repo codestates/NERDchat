67d05acbd33751126667d4ffd66a55b5d0a05cff
Mangpha <8350130@gmail.co>
2021-09-09 02:57:43 +0900

    [SR] Fix: Server Auto Deploy

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo chmod -R 777 /home/ubuntu/server
-cd /home/ubuntu/server
+sudo chmod -R 777 /home/ubuntu/
+cd /home/ubuntu/
 pm2 stop index 2> /dev/null || true
 pm2 delete index 2> /dev/null || true
