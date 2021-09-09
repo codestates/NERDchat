cd50b3cb2a214a4e817a77eb891887f928d1923a
Mangpha <8350130@gmail.co>
2021-09-09 13:43:41 +0900

    [SR] Fix: AUto Deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,6 @@
 sudo chmod -R 777 /home/ubuntu/server
 cd /home/ubuntu/server
 npm i
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080
 authbind --deep pm2 start index.js --watch
