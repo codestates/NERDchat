f81172d84ae85730936782ea9efa5545302afbdb
Mark <notbibu@gmail.com>
2021-09-14 07:54:00 +0900

    [Client]Add:install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import HomePage from './pages/hompage/Homepage';
-import Login from './components/login/Login';
 function App () {
   return (
     <div>
-      <Login />
       <HomePage />
     </div>
   );
