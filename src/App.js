23abb2f6eee4777b0254e732d525aaa30e3a1aa1
xinnni <79839230+xinnni@users.noreply.github.com>
2021-09-13 21:13:45 +0900

    [Client] Add: Nav component (#146)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import HomePage from './pages/hompage/Homepage';
 function App () {
   return (
-    <div />
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './_Footer.scss';
+
+const Footer = () => {
+  return <div>나는 푸터</div>;
+};
+
+export default Footer;
