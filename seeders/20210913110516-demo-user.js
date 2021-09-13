a8eb0bb638e511d2a3f443a9af270673a5273c43
lotus0204 <lotusrb0204@gmail.com>
2021-09-13 20:52:38 +0900

    [Server]Add:seed

--- seeders/20210913110516-demo-user.js
+++ seeders/20210913110516-demo-user.js
@@ -3,17 +3,21 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     
-      // await queryInterface.bulkInsert('Users', [{
-      //   id: 0,
-      //   userId: 'tnvjakzpt',
-      //   avatar: null,
-      //   email: 'tnvjakzpt@nate.com',
-      //   nickname: 'tnvjakzpt',
-      //   password: '1234',
-      //   valid: true,
-      //   oauth: null,
-      //   status:
-      // }]);
+      await queryInterface.bulkInsert('Users', [{
+        id: 0,
+        userId: 'tnvjakzpt',
+        avatar: null,
+        email: 'tnvjakzpt@nate.com',
+        nickname: 'tnvjakzpt',
+        password: '1234',
+        valid: true,
+        oauth: null,
+        status: null,
+        currentRoom: '1',
+        superuser: true,
+        createdAt: new Date(),
+				updatedAt: new Date(), 
+      }]);
     
   },
 
