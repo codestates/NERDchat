6f864b86f0542f85d7ef7d0c3eefb58680850412
lotus0204 <lotusrb0204@gmail.com>
2021-09-13 20:53:45 +0900

    [Server]Fix:semistandard

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -7,21 +7,20 @@ module.exports = async (req, res) => {
   const userData = await Users.findOne({
     where: { userId: id }
   });
-  if (!userData) res.status(404).json({ isLogin: false, message: "user not found" });
-  else if (!comparePassword(password,userData.password)) res.status(400).json({ isLogin: false });
+  if (!userData) res.status(404).json({ isLogin: false, message: 'user not found' });
+  else if (!comparePassword(password, userData.password)) res.status(400).json({ isLogin: false });
   else {
     const { id, avatar, userId, nickname, email, oauth, status } = userData;
     const accessToken = generateAccess({ email, nickname });
     const refreshToken = generateRefresh({ email, nickname });
 
-    res.set({ 'refreshToken': refreshToken });
+    res.set({ refreshToken: refreshToken });
     res.status(200).json({
       data: { accessToken, id, avatar, userId, nickname, email, oauth, status },
       isLogin: true
     });
-
   }
-  
+
   // Search User Data
   /*
     const userData = await Users.findOne({where: {userId: id}});
--- seeders/20210913110516-demo-user.js
+++ seeders/20210913110516-demo-user.js
@@ -2,23 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
-      await queryInterface.bulkInsert('Users', [{
-        id: 0,
-        userId: 'tnvjakzpt',
-        avatar: null,
-        email: 'tnvjakzpt@nate.com',
-        nickname: 'tnvjakzpt',
-        password: '1234',
-        valid: true,
-        oauth: null,
-        status: null,
-        currentRoom: '1',
-        superuser: true,
-        createdAt: new Date(),
-				updatedAt: new Date(), 
-      }]);
-    
+    await queryInterface.bulkInsert('Users', [{
+      id: 0,
+      userId: 'tnvjakzpt',
+      avatar: null,
+      email: 'tnvjakzpt@nate.com',
+      nickname: 'tnvjakzpt',
+      password: '1234',
+      valid: true,
+      oauth: null,
+      status: null,
+      currentRoom: '1',
+      superuser: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
   },
 
   down: async (queryInterface, Sequelize) => {
