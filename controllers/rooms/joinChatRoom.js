f375c3a4206de25d436085f6bd3ee44f21b75012
Mangpha <78544031+Mangpha@users.noreply.github.com>
2021-09-14 14:22:26 +0900

    [Server] Add: Join Chat, Exit Chat, Create Chat & Fix: Routes & Add: Room Seeds (#162)
    * [Server] Add: Room Controllers
    
    * [Server] Fix: Route
    
    * [Server] Fix: Token Verify
    
    * [Server] Fix: Semistandard
    
    * [Server] Add: Join Chat, Exit Chat, Create Chat & Fix: Routes & Add: Room Seeds

--- controllers/rooms/joinChatRoom.js
+++ controllers/rooms/joinChatRoom.js
@@ -5,17 +5,22 @@ const io = require('socket.io')();
 
 module.exports = async (req, res) => {
   const { uuid } = req.params;
-  const { authentication } = req.headers;
-  if (authentication) res.status(401).json({ message: 'token not found' });
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) res.status(401).json({ message: 'token not found' });
   else {
-    const userData = verifyAccess(authentication);
+    const userData = verifyAccess(token);
     if (!userData) res.status(400).json({ message: 'token expired' });
     else {
-      const chatRoomData = await GameChatRooms.findOne({ where: { uuid } });
-      if (io.sockets.manager.rooms[uuid].length <= chatRoomData.max) res.status(403).json({ message: 'full' });
-      else {
-        await Users.update({ currentRoom: uuid }, { where: { id: userData.id } });
-        res.status(200).json({ data: { userId: userData.id, roomId: chatRoomData.id, createdAt: chatRoomData.createdAt } });
+      try {
+        const chatRoomData = await GameChatRooms.findOne({ where: { uuid } });
+        if (!chatRoomData || !io.sockets.adapter.rooms[uuid]) res.status(404).json({ message: 'room not found' });
+        if (io.sockets.adapter.rooms[uuid].length <= chatRoomData.max) res.status(403).json({ message: 'full' });
+        else {
+          await Users.update({ currentRoom: uuid }, { where: { id: userData.id } });
+          res.status(200).json({ data: { userId: userData.id, roomId: chatRoomData.id, createdAt: chatRoomData.createdAt } });
+        }
+      } catch (err) {
+        console.log(err);
       }
     }
   }
