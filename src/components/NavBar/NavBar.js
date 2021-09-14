c5d4747f6af91225ce493596dff60dbd82343ac5
xinnni <79839230+xinnni@users.noreply.github.com>
2021-09-14 09:02:00 +0900

    [Client] Fix: Nav component  (#157)
    * [Client] Add: Nav component
    
    * [Client] Fix: Nav component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,46 +1,68 @@
-import React, { useState } from 'react';
-import { AiOutlineBars, AiOutlineSearch, AiOutlineEnter } from 'react-icons/ai';
-import { ReactComponent as Glasses } from '../../images/glasses.svg';
-import './_NavBar.scss';
+import React, { useState } from "react";
+import { AiOutlineBars, AiOutlineSearch, AiOutlineEnter } from "react-icons/ai";
+import { ReactComponent as Glasses } from "../../images/glasses.svg";
+
+import "./_NavBar.scss";
 
 const NavBar = () => {
+  // const [checked, setChecked] = useState(false);
+
+  // const clickHandler = () => {
+  //   setChecked((prev) => !prev);
+  // };
   return (
-    <div className='nav'>
-      <nav className='nav_bar'>
-        <label htmlFor='nav_showmenu' className='nav_menu'>
-          <AiOutlineBars size={25} />
+    <div className="nav_wrapper">
+      <nav>
+        <input type="checkbox" id="show-search" />
+        <input type="checkbox" id="show-menu" />
+        <label htmlFor="show-menu" className="nav_menuIcon">
+          <AiOutlineBars size={20} />
         </label>
-        <input type='checkbox' id='nav_showmenu' />
-        <div className='nav_container'>
-          <div className='nav_logo'>
-            <a href='/'>
-              <Glasses width='110' height='110' />
+        <div className="nav_content">
+          <div className="nav_logo">
+            <a href="/">
+              <Glasses width="150" height="150" />
             </a>
-            {/* <span>NERDchat</span> */}
           </div>
-          <ul className='nav_links'>
+          <ul className="nav_links">
             <li>
-              <a href='/'>SERVER</a>
+              <a href="/">SERVER</a>
             </li>
             <li>
-              <a href='/'>LOGIN</a>
+              <a href="/">BOOKMARK</a>
+              <ul>
+                <li>
+                  <a href="/">Drop Menu 1</a>
+                </li>
+                <li>
+                  <a href="/">Drop Menu 2</a>
+                </li>
+                <li>
+                  <a href="/">Drop Menu 3</a>
+                </li>
+                <li>
+                  <a href="/">Drop Menu 4</a>
+                </li>
+                <li>
+                  <a href="/">Drop Menu 5</a>
+                </li>
+              </ul>
             </li>
             <li>
-              <a href='/'>BOOKMARK</a>
+              <a href="/">LOGIN</a>
             </li>
             <li>
-              <a href='/'>SIGN UP</a>
+              <a href="/">SIGN IN</a>
             </li>
           </ul>
         </div>
-        <input type='checkbox' id='nav_showsearch' />
-        <label htmlFor='nav_showsearch' className='nav_search'>
-          <AiOutlineSearch size={25} />
+        <label htmlFor="show-search" className="nav_searchIcon">
+          <AiOutlineSearch size={20} />
         </label>
-        <form className='nav_searchbox'>
-          <input type='text' placeholder='Search the server' required />
-          <button type='submit' className='nav_enter'>
-            <AiOutlineEnter size={25} className='nav_i_enter' />
+        <form action="/" className="nav_searchBox">
+          <input type="text" placeholder="Something to Search..." required />
+          <button type="submit" className="nav_goIcon">
+            <AiOutlineEnter size={20} />
           </button>
         </form>
       </nav>
