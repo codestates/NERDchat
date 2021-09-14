b2a10739bd8a065a70f6d73532481649e9917e84
lotus0204 <lotusrb0204@gmail.com>
2021-09-14 11:16:19 +0900

    [Server]Add:users

---
+++ controllers/users/profile.js
@@ -0,0 +1,22 @@
+const { Users } = require('../../models');
+
+module.exports = async (req, res) => {
+  try {
+    const userInfo = await Users.findOne({ where: { nickname: req.params.nickname } });
+    if (!userInfo) res.status(404).json({ profile: "not found" });
+    else {
+      const { avatar, userId, nickname, email, status } = userInfo;
+      res.status(302).json({
+        data: {
+          avatar,
+          userId,
+          nickname,
+          email,
+          status
+        }
+      });
+    }   
+  } catch (err) {
+    console.log(err);
+  }
+}
