bd331f62e9061a7f03158fc604b0e7ae90a91317
Mark <notbibu@gmail.com>
2021-09-14 19:33:23 +0900

    [Client]to adjust change

--- src/UI/bookmark/Bookmark.js
+++ src/UI/bookmark/Bookmark.js
@@ -1,24 +1,23 @@
-import {useState} from 'react'
+import { useState } from 'react';
 import { IoBookmarkOutline } from 'react-icons/io5';
 const Bookmark = () => {
-    const [hover, setHover] = useState(false);
-    const onHover = () => {
-        setHover(true);
-      };
-    
-      const onLeave = () => {
-        setHover(false);
-      };
-    return (
-        <div
-        onMouseEnter={onHover}
-        onMouseLeave={onLeave}
-        role="button"
-        // tabIndex="-3"
-        >
-            {hover? <IoBookmarkOutline color="rgb(184, 126, 255)" size={25}/> : <IoBookmarkOutline color='white' size={25}/>}
-        </div>
-    )
-}
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(true);
+  };
 
-export default Bookmark
+  const onLeave = () => {
+    setHover(false);
+  };
+  return (
+    <div
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+      role='button'
+    >
+      {hover ? <IoBookmarkOutline color='rgb(184, 126, 255)' size={25} /> : <IoBookmarkOutline color='white' size={25} />}
+    </div>
+  );
+};
+
+export default Bookmark;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { AiOutlineBars, AiOutlineSearch, AiOutlineEnter } from "react-icons/ai";
-import { ReactComponent as Glasses } from "../../images/glasses.svg";
+import React, { useState } from 'react';
+import { AiOutlineBars, AiOutlineSearch, AiOutlineEnter } from 'react-icons/ai';
+import { ReactComponent as Glasses } from '../../images/glasses.svg';
 
-import "./_NavBar.scss";
+import './_NavBar.scss';
 
 const NavBar = () => {
   // const [checked, setChecked] = useState(false);
@@ -11,57 +11,57 @@ const NavBar = () => {
   //   setChecked((prev) => !prev);
   // };
   return (
-    <div className="nav_wrapper">
+    <div className='nav_wrapper'>
       <nav>
-        <input type="checkbox" id="show-search" />
-        <input type="checkbox" id="show-menu" />
-        <label htmlFor="show-menu" className="nav_menuIcon">
+        <input type='checkbox' id='show-search' />
+        <input type='checkbox' id='show-menu' />
+        <label htmlFor='show-menu' className='nav_menuIcon'>
           <AiOutlineBars size={20} />
         </label>
-        <div className="nav_content">
-          <div className="nav_logo">
-            <a href="/">
-              <Glasses width="150" height="150" />
+        <div className='nav_content'>
+          <div className='nav_logo'>
+            <a href='/'>
+              <Glasses width='150' height='150' />
             </a>
           </div>
-          <ul className="nav_links">
+          <ul className='nav_links'>
             <li>
-              <a href="/">SERVER</a>
+              <a href='/'>SERVER</a>
             </li>
             <li>
-              <a href="/">BOOKMARK</a>
+              <a href='/'>BOOKMARK</a>
               <ul>
                 <li>
-                  <a href="/">Drop Menu 1</a>
+                  <a href='/'>Drop Menu 1</a>
                 </li>
                 <li>
-                  <a href="/">Drop Menu 2</a>
+                  <a href='/'>Drop Menu 2</a>
                 </li>
                 <li>
-                  <a href="/">Drop Menu 3</a>
+                  <a href='/'>Drop Menu 3</a>
                 </li>
                 <li>
-                  <a href="/">Drop Menu 4</a>
+                  <a href='/'>Drop Menu 4</a>
                 </li>
                 <li>
-                  <a href="/">Drop Menu 5</a>
+                  <a href='/'>Drop Menu 5</a>
                 </li>
               </ul>
             </li>
             <li>
-              <a href="/">LOGIN</a>
+              <a href='/'>LOGIN</a>
             </li>
             <li>
-              <a href="/">SIGN IN</a>
+              <a href='/'>SIGN IN</a>
             </li>
           </ul>
         </div>
-        <label htmlFor="show-search" className="nav_searchIcon">
+        <label htmlFor='show-search' className='nav_searchIcon'>
           <AiOutlineSearch size={20} />
         </label>
-        <form action="/" className="nav_searchBox">
-          <input type="text" placeholder="Something to Search..." required />
-          <button type="submit" className="nav_goIcon">
+        <form action='/' className='nav_searchBox'>
+          <input type='text' placeholder='Something to Search...' required />
+          <button type='submit' className='nav_goIcon'>
             <AiOutlineEnter size={20} />
           </button>
         </form>
