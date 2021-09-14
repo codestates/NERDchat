557eb6295f79c45409a72f4b108a5166c16778e6
Mangpha <78544031+Mangpha@users.noreply.github.com>
2021-09-14 14:25:22 +0900

    Revert "[Server] Add: Join Chat, Exit Chat, Create Chat & Fix: Routes & Add: Room Seeds (#162)"
    This reverts commit f375c3a4206de25d436085f6bd3ee44f21b75012.

--- controllers/rooms/exitChatRoom.js
+++ controllers/rooms/exitChatRoom.js
@@ -1,29 +1,3 @@
-const { GameChatRooms } = require('../../models');
-const { Users } = require('../../models');
-const { verifyAccess } = require('../../middlewares/token');
-const io = require('socket.io')();
-
 module.exports = async (req, res) => {
-  const { uuid } = req.params;
-  const token = req.headers.authorization.split(' ')[1];
-  if (!token) res.status(401).json({ message: 'token not found' });
-  else {
-    const userData = verifyAccess(token);
-    console.log(userData);
-    if (!userData) res.status(400).json({ message: 'token expired' });
-    else {
-      try {
-        const usersRoomId = await Users.findOne({ where: { id: userData.id } });
-        if (uuid !== usersRoomId.currentRoom) res.status(500).json({ message: 'different uuid' });
-        else {
-          await Users.update({
-            currentRoom: null
-          }, { where: { id: userData.id } });
-          res.status(200).json({ message: 'exit' });
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  }
+
 };
