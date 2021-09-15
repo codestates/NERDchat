640f56a856deaae09f227184a57187b053dd15bb
Mangpha <8350130@gmail.co>
2021-09-15 14:31:11 +0900

    [Server] Fix: Token => refactoring code

--- controllers/rooms/joinChatRoom.js
+++ controllers/rooms/joinChatRoom.js
@@ -5,23 +5,19 @@ const io = require('socket.io')();
 
 module.exports = async (req, res) => {
   const { uuid } = req.params;
-  const token = req.headers.authorization.split(' ')[1];
-  if (!token) res.status(401).json({ message: 'token not found' });
-  else {
-    const userData = verifyAccess(token);
-    if (!userData) res.status(400).json({ message: 'token expired' });
+  const userData = verifyAccess(req, res);
+  if (!userData) {
+    return res.status(400).send('Error');
+  }
+  try {
+    const chatRoomData = await GameChatRooms.findOne({ where: { uuid } });
+    if (!chatRoomData || !io.sockets.adapter.rooms[uuid]) res.status(404).json({ message: 'room not found' });
+    if (io.sockets.adapter.rooms[uuid].length <= chatRoomData.max) res.status(403).json({ message: 'full' });
     else {
-      try {
-        const chatRoomData = await GameChatRooms.findOne({ where: { uuid } });
-        if (!chatRoomData || !io.sockets.adapter.rooms[uuid]) res.status(404).json({ message: 'room not found' });
-        if (io.sockets.adapter.rooms[uuid].length <= chatRoomData.max) res.status(403).json({ message: 'full' });
-        else {
-          await Users.update({ currentRoom: uuid }, { where: { id: userData.id } });
-          res.status(200).json({ data: { userId: userData.id, roomId: chatRoomData.id, createdAt: chatRoomData.createdAt } });
-        }
-      } catch (err) {
-        console.log(err);
-      }
+      await Users.update({ currentRoom: uuid }, { where: { id: userData.id } });
+      res.status(200).json({ data: { userId: userData.id, roomId: chatRoomData.id, createdAt: chatRoomData.createdAt } });
     }
+  } catch (err) {
+    console.log(err);
   }
 };
--- middlewares/token.js
+++ middlewares/token.js
@@ -4,19 +4,30 @@ require('dotenv').config();
 module.exports = {
   generateAccess: (payload) => jwt.sign(payload, process.env.ACCESS_SECRET, { expiresIn: '1d' }),
   generateRefresh: (payload) => jwt.sign(payload, process.env.REFRESH_SECRET, { expiresIn: '7d' }),
-  verifyAccess: (accessToken) => {
-    try {
-      return jwt.verify(accessToken, process.env.ACCESS_SECRET);
-    } catch (err) {
-      return null;
+  verifyAccess: (req, res) => {
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) res.status(401).json({ message: 'token not found' });
+    else {
+      try {
+        const userData = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+        return userData;
+      } catch (err) {
+        res.status(400).json({ message: 'token exipred' });
+        return null;
+      }
     }
   },
-  verifyRefresh: (refreshToken) => {
-    try {
-      return jwt.verify(refreshToken, process.env.REFRESH_SECRET);
-    } catch (err) {
-      return null;
+  verifyRefresh: (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) res.status(401).json({ message: 'token not found' });
+    else {
+      try {
+        const userData = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+        return userData;
+      } catch (err) {
+        res.status(400).json({ message: 'token exipred' });
+        return null;
+      }
     }
   }
-
 };
