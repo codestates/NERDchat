feacbcc9f2a5f7715bfb16fa53127d237969d4d6
xinnni <79839230+xinnni@users.noreply.github.com>
2021-09-15 17:55:06 +0900

    [Client] Fix: ui (#188)
    * [Client] Add: Nav component
    
    * [Client] Fix: Nav component
    
    * [Client] Fix: nav component
    
    * [Client] Fix: first page components
    
    * [Client] Fix: main page component and slide component
    
    * [Client] Fix: ui
    
    * [Client] Fix: ui
    
    * [Client] Fix: ui

--- src/components/Hero/HeroSecond.js
+++ src/components/Hero/HeroSecond.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as Sec } from "../../images/SVG/streamline-icon-video-conference@250x250.svg";
+import { ReactComponent as Sec } from "../../images/SVG/streamline-icon-overworked-employee@250x250.svg";
 
 import "./HeroSecond.scss";
 
@@ -9,6 +9,24 @@ const HeroSecond = () => {
       <div className="herosec-container">
         <div className="herosec-logoimage">
           <Sec width="680" height="680" />
+        </div>
+        <div className="text-container">
+          <div className="text-ani">
+            <span className="first">M</span>
+            <span className="first">E</span>
+            <span className="first">E</span>
+            <span className="first">T</span>
+            <span className="slide">
+              <h1 className="second-new">NEW</h1>
+              <h1 className="second-fri">FRIENDS</h1>
+            </span>
+          </div>
+          <div className="sub-text">
+            <h1>
+              Did you meet a new friend? enjoy the game with them. If you do it
+              together, it will be more fun. Anything is possible on NerdChat.
+            </h1>
+          </div>
         </div>
       </div>
     </div>
