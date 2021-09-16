7de3542ad110345839e9af7aa4907493426062a8
HTlee1990 <notbibu@gmail.com>
2021-09-16 21:10:25 +0900

    [Client]Add: infinite Scroll to room lists (#200)
    * [Client]basic setting
    
    * [Client]Add: basic settings
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]install axios
    
    * [Client]Add: send request from login/signUp
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add: ServerPage
    
    * [Client]to adjust change
    
    * [Client]Add:Context and connect modal to header
    
    * [Client]Add Context
    
    * [Client]Fix:conflict
    
    * [Client]Add: say hello to server
    
    * [Client]Fix:conflict
    
    * [Client]Add: ServerRoom Component
    
    * [Client]Add: infiniteScroll to room list

--- src/pages/ServerRoomPage/ServerRoom.js
+++ src/pages/ServerRoomPage/ServerRoom.js
@@ -1,45 +1,16 @@
 import { useState, useRef, useCallback } from 'react'
 import { useParams } from 'react-router-dom';
 import './ServerRoom.scss'
-import ServerRoomCard from '../../components/ServerRoomCard/ServerRoomCard'
-import useLists from '../../hooks/useLists'
-
+import ServerRoomBody from '../../components/ServerRoom/ServerRoomBody/ServerRoomBody'
+import ServerRoomHeader from '../../components/ServerRoom/ServerRoomHeader/ServerRoomHeader'
 const ServerRoom = () => {
-    const {gameId} = useParams();
-    // const { lists, hasMore, loading} = useLists(pageNum, "post", "/rooms/list/", { gameId });
-    const [pageNum, setPageNum] = useState(1);
-    // const observer = useRef();
-
-    // const lastElementRef = useCallback(node => {
-    //     if(loading) return;
-    //     if(observer.current) observer.current.disconnect();
-    //     observer.current = new IntersectionObserver(entries => {
-    //         if(entries[0].isIntersecting && hasMore){
-    //             setPageNum((prevPage) => prevPage + 1);
-    //         }
-    //     })
-    //     if(node) observer.current.observe(node);
-    // }, [loading, hasMore])
-    const lists = [{id: 1, roomTitle: 'heyguys', uuid:13124, max: 3}, {id: 2, roomTitle: 'how ya doing', uuid:1123123, max: 5}];
-    //현재 인원수는 io.sockets.adapter.rooms[uuid] 로 받아오자.
-
     return (
-        <div className="big__container">
-            <div className="roomLists__container">
-                {lists.map((list, idx) => {
-                    // if(lists.length === idx + 1){
-                    //     return (<div key={list.id} ref={lastElementRef}>
-                    //         <ServerRoomCard id={list.id} roomTitle={list.roomTitle} uuid={list.uuid} max={list.max}/>
-                    //     </div>)
-                    // }
-                    // else{
-                        return (<div key={list.id} >
-                            <ServerRoomCard id={list.id} roomTitle={list.roomTitle} uuid={list.uuid} max={list.max}/>
-                        </div>)
-                    // }
-                })}
-                {/* <div>{loading && 'loading...'}</div> */}
-            </div>
+        
+        <div className="server__room__container">
+                <div className="room__content">
+                    <ServerRoomHeader/>
+                    <ServerRoomBody/>
+                </div>
         </div>
     )
 }
