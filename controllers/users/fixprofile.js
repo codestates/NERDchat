add6ac5071a2096aa1839fb81c6423dda0a183d1
lotus0204 <lotusrb0204@gmail.com>
2021-09-16 13:54:23 +0900

    [Server]Fix:multer

--- controllers/users/fixprofile.js
+++ controllers/users/fixprofile.js
@@ -1,53 +1,49 @@
 const { Users } = require('../../models');
 const { generateAccess, verifyAccess } = require('../../middlewares/token');
-const { comparePassword, generatePassword } = require('../../middlewares/crypto');
-// const { uploadImage } = require('../../middlewares/multer');
+const { generatePassword } = require('../../middlewares/crypto');
 
 module.exports = async (req, res) => {
-  const originAccessToken = req.headers.authorization.split(' ')[1];
-  if (!originAccessToken) res.status(401).json({ message: 'token not found' });
-  else {
-    const accessTokenData = verifyAccess(originAccessToken);
-    if (!accessTokenData) res.status(400).json({ message: 'token expired' });
-    else {
-      try {
-        const { avatar, nickname, password, status } = req.body;
-        const origin = await Users.findOne({
-          where: { email: accessTokenData.email, nickname: accessTokenData.nickname }
-        });
-        // if (!comparePassword(password, origin.password)) res.status(403).json({ message: 'passwords are different' });
-        const newPassword = generatePassword(password);
-        await Users.update({
-          avatar: avatar || origin.avatar,
-          nickname: nickname || origin.nickname,
-          password: newPassword || origin.password,
-          status: status || origin.status,
-          updatedAt: new Date()
-        },
-        {
-          where: {
-            id: origin.id
-          }
-        });
-        // uploadImage(avatar);
-        const accessToken = generateAccess({
-          email: origin.email,
-          nickname: nickname || origin.nickname
-        });
-        res.status(202).json({
-          data: {
-            accessToken,
-            id: origin.id,
-            avatar,
-            userId: origin.userId,
-            nickname,
-            email: origin.email,
-            status
-          }
-        });
-      } catch (err) {
-        console.log(err);
-      }
+  const userData = verifyAccess(req, res);
+  const { nickname, password, status } = req.body;
+  const avatar = req.files[0].filename;
+  try {
+    const origin = await Users.findOne({
+      where: { email: userData.email, nickname: userData.nickname }
+    });
+    if (password) {
+      var newPassword = generatePassword(password);
     }
+    await Users.update({
+      avatar: avatar || origin.avatar,
+      nickname: nickname || origin.nickname,
+      password: newPassword || origin.password,
+      status: status || origin.status,
+      updatedAt: new Date()
+    },
+    {
+      where: {
+        id: origin.id
+      }
+    });
+    const accessToken = generateAccess({
+      email: origin.email,
+      nickname: nickname || origin.nickname
+    });
+    const expireDate = new Date(Date.now() + 60 * 60 * 1000 * 24);
+
+    res.cookie('accessToken', accessToken, { httpOnly: true, expires: expireDate, sameSite: 'none', secure: true })
+      .status(202).json({
+        data: {
+          accessToken,
+          id: origin.id,
+          avatar: avatar || origin.avatar,
+          userId: origin.userId,
+          nickname: nickname || origin.nickname,
+          email: origin.email,
+          status: status || origin.status
+        }
+      });
+  } catch (err) {
+    console.log(err);
   }
 };
