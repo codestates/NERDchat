9cbb7c5f39823a023362143013a0d0040dcfd816
xinnni <79839230+xinnni@users.noreply.github.com>
2021-09-19 14:12:38 +0900

    [Client] Fix: socket.io bug (#223)
    * [Client] Add: Nav component
    
    * [Client] Fix: Nav component
    
    * [Client] Fix: nav component
    
    * [Client] Fix: first page components
    
    * [Client] Fix: main page component and slide component
    
    * [Client] Fix: ui
    
    * [Client] Fix: ui
    
    * [Client] Fix: ui
    
    * [Client] Add: Chatpage component
    
    * [Client] Add: Chat component
    
    * [Client] Fix: socket.io-client
    
    * [Client] Fix: socket.io bug fix

--- src/components/ServerRoom/ServerRoom.js
+++ src/components/ServerRoom/ServerRoom.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import ServerRoomBody from "./ServerRoomBody/ServerRoomBody";
-import ServerRoomHeader from "./ServerRoomHeader/ServerRoomHeader";
-import ServerSearch from "./ServerSerch/ServerSearch";
-import useSocket from "../../hooks/useSocket";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import ServerRoomBody from './ServerRoomBody/ServerRoomBody';
+import ServerRoomHeader from './ServerRoomHeader/ServerRoomHeader';
+import ServerSearch from './ServerSerch/ServerSearch';
+import useSocket from '../../hooks/useSocket';
+import { useParams } from 'react-router-dom';
 
 const ENDPOINT = process.env.REACT_APP_ENDPOINT;
 
@@ -19,7 +19,7 @@ const ServerRoom = () => {
     setSearchedLists(res.data);
     setIsSearched(1);
   };
-  const { message, sendMessage } = useSocket(gameId, "");
+  const { message, sendMessage } = useSocket(gameId, '');
 
   return (
     <>
--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,33 +1,32 @@
-import { useEffect, useState, useRef } from "react";
-import { io } from "socket.io-client";
+import { useEffect, useState, useRef } from 'react';
+import { io } from 'socket.io-client';
 
+// serverRoom.js, Chat.js에서 사용중
 const useSocket = (serverName, roomId, userInfo) => {
   console.log(`This is ServerName, ${serverName}, ${roomId}, ${userInfo}`);
-  const [message, setMessage] = useState("");
+  console.log(userInfo);
+  const [messages, setMessages] = useState([]);
   const socket = useRef();
 
-  const sendMessage = (roomId, chatId, userInfo, newMsg) => {
-    console.log(roomId, chatId, userInfo, newMsg);
-    socket.current.emit("roomMessage", roomId, chatId, userInfo, newMsg);
-  };
-  const getMessage = () => {
-    // console.log('getmessage')
-    // socket.current.on('roomMessage', (data) => console.log(data))
-  };
-  const joinRoom = () => {
-    console.log();
-    socket.current.emit("joinRoom", roomId, 123, userInfo, `welcome!!!`);
-    console.log("joinRoom activated");
-  };
   useEffect(() => {
-    //serverName = nameSpace, roomId = roomId
-    //serverName이라는 ns로 접속하고, socket.current에 배당.
+    // serverName = nameSpace, roomId = roomId
+    // serverName이라는 ns로 접속하고, socket.current에 배당.
     socket.current = io(`http://localhost:8080/${serverName}`, {
-      query: { serverName, roomId },
+      query: { serverName, roomId }
     });
-    socket.current.on("roomMessage", (data) => {
-      console.log(123);
-      console.log(data);
+    socket.current.on('welcomeRoom', (userData, msgData) =>
+      console.log(userData, msgData)
+    );
+    // 서버쪽에서 객체형태로 메시지를 넘겨줄때는 아래처럼 하나의 인자로 받을 수 있지만,
+    // 만약 객체로 묶지 않고 여러개의 인자로 넘겨줄때는 위처럼 2개의 인자로 나누어 받아야 한다.
+    socket.current.on('roomMessage', (data) => {
+      // console.log(data);
+      const incomingMsg = {
+        body: data.msgData,
+        mine: data.userData.userId === userInfo.userId
+      };
+      console.log(incomingMsg);
+      setMessages((prev) => [...prev, incomingMsg]);
     });
     return () => {
       socket.current.close();
@@ -35,7 +34,17 @@ const useSocket = (serverName, roomId, userInfo) => {
     };
   }, []);
 
-  return { joinRoom, message, sendMessage, getMessage };
+  const sendMessage = (roomId, chatId, userInfo, newMsg) => {
+    // console.log(roomId, chatId, userInfo, newMsg);
+    socket.current.emit('roomMessage', roomId, chatId, userInfo, newMsg);
+  };
+
+  const joinRoom = () => {
+    console.log();
+    socket.current.emit('joinRoom', roomId, 123, userInfo, 'welcome!!!');
+    console.log('joinRoom activated');
+  };
+  return { joinRoom, messages, sendMessage };
 };
 
 export default useSocket;
