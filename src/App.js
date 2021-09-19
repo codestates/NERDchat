120b3e119356204af689cf930d0743156918d004
xinnni <79839230+xinnni@users.noreply.github.com>
2021-09-19 14:10:53 +0900

    [Client] Fix: socket.io bug fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import "./App.css";
-import HomePage from "./pages/HomePage/Homepage";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import ChatPage from "./pages/ChatPage/ChatPage";
-import ServerPage from "./pages/serverpage/ServerPage";
-import ServerRoomPage from "./pages/ServerRoomPage/ServerRoomPage";
-import Mypage from "./pages/MyPage/MyPage";
+import './App.css';
+import HomePage from './pages/HomePage/Homepage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ChatPage from './pages/ChatPage/ChatPage';
+import ServerPage from './pages/serverpage/ServerPage';
+import ServerRoomPage from './pages/ServerRoomPage/ServerRoomPage';
+import Mypage from './pages/MyPage/MyPage';
 
-function App() {
+function App () {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <HomePage />
         </Route>
-        <Route path="/servers">
+        <Route path='/servers'>
           <ServerPage />
         </Route>
-        <Route exact path="/gameId=:gameId">
+        <Route exact path='/gameId=:gameId'>
           <ServerRoomPage />
         </Route>
-        <Route path="/gameId=:gameId/roomId=:roomId/chatId=:chatId">
+        <Route path='/gameId=:gameId/roomId=:roomId/chatId=:chatId'>
           <ChatPage />
         </Route>
-        <Route exact path="/mypage">
+        <Route exact path='/mypage'>
           <Mypage />
         </Route>
       </Switch>
