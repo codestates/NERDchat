1eaefe27d533557175153bfc7bca01106659a195
xinnni <79839230+xinnni@users.noreply.github.com>
2021-09-20 23:23:29 +0900

    [Client] Fix: socket.io-client

--- src/components/NavBar/NavItem.js
+++ src/components/NavBar/NavItem.js
@@ -1,15 +1,15 @@
-import React, { useState, useContext, useEffect, useRef } from 'react';
+import React, { useState, useContext, useEffect, useRef } from "react";
 import {
   IoGameControllerOutline,
   IoBookOutline,
   IoFingerPrintOutline,
-  IoConstructOutline
-} from 'react-icons/io5';
+  IoConstructOutline,
+} from "react-icons/io5";
 
-import './NavItem.scss';
-import DropdownMenu from './DropdownMenu';
-import Login from '../../components/login/Login';
-import { Context } from '../../context/ContextProvider';
+import "./NavItem.scss";
+import DropdownMenu from "./DropdownMenu";
+import Login from "../../components/login/Login";
+import { Context } from "../../context/ContextProvider";
 
 const NavItem = () => {
   // const dropEl = useRef();
@@ -32,18 +32,18 @@ const NavItem = () => {
   return (
     <>
       {loginModalOpen && <Login />}
-      <li className='nav-item'>
-        <a href='/' className='icon-button'>
+      <li className="nav-item">
+        <a href="/servers" className="icon-button">
           <IoGameControllerOutline size={30} />
         </a>
-        <a className='icon-button' onClick={openHandler}>
+        <a className="icon-button" onClick={openHandler}>
           <IoBookOutline size={30} />
         </a>
-        {open && <DropdownMenu />}
-        <a className='icon-button' onClick={loginmodalHandler}>
+        {/* {open && <DropdownMenu />} */}
+        <a className="icon-button" onClick={loginmodalHandler}>
           <IoFingerPrintOutline size={30} />
         </a>
-        <a href='/' className='icon-button'>
+        <a href="/" className="icon-button">
           <IoConstructOutline size={30} />
         </a>
       </li>
