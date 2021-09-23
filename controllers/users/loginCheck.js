6cec32051a500f3b7377c70864a1e04735316278
Mangpha <78544031+Mangpha@users.noreply.github.com>
2021-09-23 10:52:23 +0900

    [Server] Add: Redis Pub,Sub Add: Relogin(check) (#228)
    * [Server] Add: GameCategory API, dummy Data, Fix: Token, Router, getList
    
    * [Server] Fix: Routes Conflict
    
    * [Server] Fix: Routes Conflict
    
    * [Server] Add: Favorites, Friends Lists
    
    * [Server] Fix: Token => refactoring code
    
    * [Server] Fix: Seed, Chat Room List
    
    * [Server] Fix: Status Code
    
    * [Server] Add: Search Room Title
    
    * [Server] Fix: Socket
    
    * [Server] Add: Redis, ReLogin(check)
    
    * [Server] Fix: Room Create
    
    * [Server] Fix: loginCheck => reLogin

--- controllers/users/loginCheck.js
+++
@@ -1,5 +0,0 @@
-const { Users } = require('../../models');
-
-module.exports = async (req, res) => {
-
-};
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,13 @@ cd /home/ubuntu/server
 npm i
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080
+
+export ACCESS_SECRET=$(aws ssm get-parameters --region us-east-1 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_NAME=$(aws ssm get-parameters --region us-east-1 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_USER=$(aws ssm get-parameters --region us-east-1 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export HOST=$(aws ssm get-parameters --region us-east-1 --names HOST --query Parameters[0].Value | sed 's/"//g')
+export REDIS_HOST=$(aws ssm get-parameters --region us-east-1 --names REDIS_HOST --query Parameters[0].Value | sed 's/"//g')
+export REFRESH_SECRET=$(aws ssm get-parameters --region us-east-1 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
+
 authbind --deep pm2 start index.js --watch
