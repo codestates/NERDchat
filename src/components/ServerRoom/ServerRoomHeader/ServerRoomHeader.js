a4b31dfd879313b71dc55976793b8baba743642c
HTlee1990 <notbibu@gmail.com>
2021-09-23 10:57:55 +0900

    [Client] Fix: place userInfo in cookie (#231)
    * [Client]basic setting
    
    * [Client]Add: basic settings
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]Add: Modal of Login and SignUp
    
    * [Client]install axios
    
    * [Client]Add: send request from login/signUp
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add:install react-router-dom
    
    * [Client]Add: ServerPage
    
    * [Client]to adjust change
    
    * [Client]Add:Context and connect modal to header
    
    * [Client]Add Context
    
    * [Client]Fix:conflict
    
    * [Client]Add: say hello to server
    
    * [Client]Fix:conflict
    
    * [Client]Add: ServerRoom Component
    
    * [Client]Add: infiniteScroll to room list
    
    * [Client]Add: roomsettingmodal
    
    * [Client]Add:socket useEffect
    
    * [Client]Fix:conflict
    
    * [Client]Fix:conflict
    
    * [Client]Add: socket-chat function
    
    * [Fix]ignore .env
    
    * [Client]Fix: userInfo to cookie
    
    * [Client]Fix: conflict

--- src/components/ServerRoom/ServerRoomHeader/ServerRoomHeader.js
+++ src/components/ServerRoom/ServerRoomHeader/ServerRoomHeader.js
@@ -1,28 +1,36 @@
-import { useContext } from 'react';
-import { IoIosArrowBack } from 'react-icons/io';
-import { Link } from 'react-router-dom';
-import { Context } from '../../../context/ContextProvider';
-import './ServerRoomHeader.scss';
-import ServerSearch from '../ServerSerch/ServerSearch';
-import RoomSetting from '../../RoomSetting/RoomSetting';
+import { useContext, useState, useEffect } from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { Context } from "../../../context/ContextProvider";
+import "./ServerRoomHeader.scss";
+import ServerSearch from "../ServerSerch/ServerSearch";
+import RoomSetting from "../../RoomSetting/RoomSetting";
+import useSocket from "../../../hooks/useSocket";
 
-const ServerRoomHeader = () => {
+const ServerRoomHeader = ({ gameId }) => {
+  const { headCount } = useSocket(gameId, "", "");
   const { createRoomOpen, createRoomModalHandler } = useContext(Context);
 
   const creatRoomHandler = () => {
     createRoomModalHandler();
   };
+
   return (
     <>
       {createRoomOpen && <RoomSetting />}
-      <div className='server__room__header__container'>
-        <Link to='/servers' className='back__icons'>
+      <div className="server__room__header__container">
+        <Link to="/servers" className="back__icons">
           <IoIosArrowBack size={30} />
           <IoIosArrowBack size={30} />
-          <h2 data-text='NERDchat'>NERDchat</h2>
+          <h2 data-text="NERDchat">NERDchat</h2>
         </Link>
-        {/* <ServerSearch/> */}
-        <button className='create__room' onClick={creatRoomHandler}>Create!</button>
+        <div className="headCount__container">
+          {`현재 접속자 수: ${headCount}`}
+        </div>
+        {/* <ServerSearch /> */}
+        <button className="create__room" onClick={creatRoomHandler}>
+          Create!
+        </button>
       </div>
     </>
   );
--- src/components/login/LoginBody.js
+++ src/components/login/LoginBody.js
@@ -1,34 +1,45 @@
-import React, { useState, useRef, useContext } from 'react';
-import axios from 'axios';
-import { FcGoogle } from 'react-icons/fc';
-import { AiFillFacebook } from 'react-icons/ai';
-import kakao from './kakao.png';
-import { Context } from '../../context/ContextProvider';
-import './LoginBody.scss';
+import React, { useState, useRef, useContext } from "react";
+import axios from "axios";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillFacebook } from "react-icons/ai";
+import kakao from "./kakao.png";
+import { useHistory } from "react-router-dom";
+import { Context } from "../../context/ContextProvider";
+import "./LoginBody.scss";
 
 const ENDPOINT = process.env.REACT_APP_ENDPOINT;
 const FACEBOOK_ID = process.env.REACT_APP_FACEBOOK_ID;
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const Login = () => {
-  const { getUserInfo, isLoginHandler, loginmodalHandler } = useContext(Context);
+  const history = useHistory();
+  const { getUserInfo, isLoginHandler, loginmodalHandler } =
+    useContext(Context);
   const idInputRef = useRef();
   const pwInputRef = useRef();
   const [err, setErr] = useState();
 
   // 일반 로그인
   const LoginHandler = async (e) => {
-    setErr('');
+    setErr("");
     e.preventDefault();
     const enteredId = idInputRef.current.value;
     const enteredPw = pwInputRef.current.value;
     if (enteredId.trim().length < 4 || enteredId.trim().length > 25) {
-      setErr('ID should be longer than 6 letters');
+      setErr("ID should be longer than 6 letters");
     } else if (enteredPw.trim().length < 6) {
-      setErr('PW should be longer than 6 letters');
+      setErr("PW should be longer than 6 letters");
     }
-    const headers = { id: enteredId, password: enteredPw, 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
-    const res = await axios.post(`${ENDPOINT}/login`, { data: null }, { headers: headers, withCredentials: true });
+    const headers = {
+      id: enteredId,
+      password: enteredPw,
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    };
+    const res = await axios.post(
+      `${ENDPOINT}/login`,
+      { data: null },
+      { headers: headers, withCredentials: true }
+    );
     // const token = res.headers.authorization
     // localStorage.setItem('ACT', token.split(' ')[1]);
     // console.log(token.split(' ')[1]);
@@ -37,6 +48,7 @@ const Login = () => {
     // login 상태 저장
     isLoginHandler(true);
     loginmodalHandler();
+    history.push("/servers");
   };
 
   // 구글 로그인
@@ -62,37 +74,43 @@ const Login = () => {
   };
   return (
     <>
-      {err && <div className='err__container'><p>{err}</p></div>}
-      <form className='login__input__container' onSubmit={LoginHandler}>
-        <div className='id__input__container'>
-          <label htmlFor='id'>아이디</label>
-          <input type='text' id='id' ref={idInputRef} />
+      {err && (
+        <div className="err__container">
+          <p>{err}</p>
         </div>
-        <div className='pw__input__container'>
-          <label htmlFor='pw'>비밀번호</label>
-          <input type='password' id='pw' ref={pwInputRef} />
+      )}
+      <form className="login__input__container" onSubmit={LoginHandler}>
+        <div className="id__input__container">
+          <label htmlFor="id">아이디</label>
+          <input type="text" id="id" ref={idInputRef} />
         </div>
-        <button type='submit' className='LoginBtn'>로그인</button>
+        <div className="pw__input__container">
+          <label htmlFor="pw">비밀번호</label>
+          <input type="password" id="pw" ref={pwInputRef} />
+        </div>
+        <button type="submit" className="LoginBtn">
+          로그인
+        </button>
       </form>
-      <div className='split__container'>
-        <div className='modal__split' />
+      <div className="split__container">
+        <div className="modal__split" />
         <p>또는</p>
-        <div className='modal__split' />
+        <div className="modal__split" />
       </div>
-      <div className='social__login'>
+      <div className="social__login">
         <button onClick={googleHandler}>
-          <div className='google'>
+          <div className="google">
             <FcGoogle size={35} />
           </div>
         </button>
         <button onClick={kakaoHandler}>
-          <div className='kakao'>
-            <img src={kakao} width='35px' alt='kakaoLogo' />
+          <div className="kakao">
+            <img src={kakao} width="35px" alt="kakaoLogo" />
           </div>
         </button>
         <button onClick={facebookHandler}>
-          <div className='facebook'>
-            <AiFillFacebook size={35} style={{ fill: '#4267B2' }} />
+          <div className="facebook">
+            <AiFillFacebook size={35} style={{ fill: "#4267B2" }} />
           </div>
         </button>
       </div>
