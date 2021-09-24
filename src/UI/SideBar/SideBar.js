82b86f9f2206ec95374f2bd1d874b4e58c64e680
xinnni <79839230+xinnni@users.noreply.github.com>
2021-09-24 17:51:06 +0900

    [Client] Fix: Peerjs and friends list

--- src/UI/SideBar/SideBar.js
+++ src/UI/SideBar/SideBar.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import FriendList from "../../components/SideTap/FriendList/FriendList";
 import Messenger from "../../components/SideTap/Messenger/Messenger";
 import OnlineUser from "../../components/SideTap/OnlineUser/OnlineUser";
+import { IoBatteryFull } from "react-icons/io5";
+
+import { Context } from "../../context/ContextProvider";
 
 import "./SideBar.scss";
 
 const SideBar = () => {
   const [toggleState, setToggleState] = useState(1);
+
+  const { friends } = useContext(Context);
+  console.log(friends);
 
   const toggleTab = (index) => {
     setToggleState(index);
@@ -38,18 +44,26 @@ const SideBar = () => {
         <div
           className={toggleState === 1 ? "content  active-content" : "content"}
         >
+          <IoBatteryFull size={30} className="content-BatteryFull" />
+          {friends.map((el) => (
+            <FriendList key={el.id} avatar={el.avatar} nickname={el.nickname} />
+          ))}
+          {/* <FriendList />
           <FriendList />
+          <FriendList />
+          <FriendList />
+          <FriendList /> */}
         </div>
         <div
           className={toggleState === 2 ? "content  active-content" : "content"}
         >
-          {/* <OnlineUser /> */}
+          <FriendList />
         </div>
 
         <div
           className={toggleState === 3 ? "content  active-content" : "content"}
         >
-          {/* <Messenger /> */}
+          <FriendList />
         </div>
       </div>
     </div>
--- src/components/SideTap/FriendList/FriendList.js
+++ src/components/SideTap/FriendList/FriendList.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { Cookies } from "react-cookie";
-import axios from "axios";
 
 import { IoBatteryFull, IoBatteryDead, IoChevronDown } from "react-icons/io5";
 import DropDown from "./DropDown/DropDown";
@@ -9,31 +8,45 @@ import "./FriendList.scss";
 
 const ENDPOINT = process.env.REACT_APP_ENDPOINT;
 
-const FriendList = () => {
+const FriendList = ({ avatar, nickname }) => {
   const cookies = new Cookies();
 
+  console.log(nickname);
+
   let userInfo = cookies.get("userInfo");
-  console.log(userInfo);
 
-  const [userName, setUserName] = useState("Nickname");
+  const [userName, setUserName] = useState(nickname);
   const [loader, setLoader] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get(`${ENDPOINT}/friends/lists`, { withCredentials: true })
-      .then((data) => console.log(data));
-  }, []);
+  const dropRef = useRef();
 
   const clickHandler = () => {
     setLoader((prev) => !prev);
   };
 
+  const backgroundCloseHandler = (e) => {
+    if (dropRef.current === e.target) {
+      setLoader(false);
+    }
+  };
+
   return (
     <>
-      <div className="friendlist__body">
+      <div
+        className="friendlist__body"
+        ref={dropRef}
+        onClick={backgroundCloseHandler}
+      >
         <div className="friendlist__body__nav">
           <div className="friendlist__name" onClick={clickHandler}>
-            {userName} <IoChevronDown size={20} className="name-icon" />
+            <img
+              src={require("../../../images/dummy/white.jpeg").default}
+              className="friendlist__avatar"
+              alt=""
+            />
+            <span className="friendlist__name-span">{userName}</span>{" "}
+            <div className="name-icon-container">
+              <IoChevronDown size={15} className="name-icon" />
+            </div>
           </div>
           {loader && <DropDown />}
         </div>
