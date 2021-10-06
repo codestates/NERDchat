71f6deed7bcf9292cb5bbd070d90cbf137621efa
xinnni <79839230+xinnni@users.noreply.github.com>
2021-10-06 10:00:31 +0900

    [Client] Fix: UI (#343)
    * [Client] Add: Nav component
    
    * [Client] Fix: Nav component
    
    * [Client] Fix: nav component
    
    * [Client] Fix: first page components
    
    * [Client] Fix: main page component and slide component
    
    * [Client] Fix: ui
    
    * [Client] Fix: ui
    
    * [Client] Fix: ui
    
    * [Client] Add: Chatpage component
    
    * [Client] Add: Chat component
    
    * [Client] Fix: socket.io-client
    
    * [Client] Fix: socket.io bug fix
    
    * [Client] Fix: bug
    
    * [Client] Fix: env
    
    * [Client] Fix: socket.io-client
    
    * [Client] Fix: socket.io-client and create dropdown
    
    * [client] Fix: socket.io-client bug
    
    * [Client] Fix: socket.io and add infinite scroll
    
    * [Client] add: peerjs and friend list
    
    * [Client] add: peerjs and friend list
    
    * [Client] Fix: Peerjs and friends list
    
    * [Client] Fix: Modal ui and axios online user
    
    * [Client] Fix: login, sign up modal ui
    
    * [Client] Fix: chat page and my page
    
    * [Client] Fix: ui and side bar
    
    * [Client] Fix: ui
    
    * [Client] Fix: socket.io
    
    * [Client] Fix: socket.io
    
    * [Client] Fix: socket.io
    
    * [Client] Fix: socket.io
    
    * [Client] Fix: socket.io
    
    * [Client] Fix: socket.io
    
    * [Client] Add: My page component and fixed UI
    
    * [Client] Fix: socket.io private chat component
    
    * [Client] Fix: socket.io
    
    * [Client] Fix: socket.io private chat
    
    * [Client] Fix: socket.io private chat
    
    * [Client] Fix: socket.io private chat
    
    * [Client] Fix: private chat component
    
    * [Client] Fix: peer.js
    
    * [Client] Add: peerjs voice component
    
    * [Client] Add: peerjs voice component
    
    * [Client] Add: peerjs voice component
    
    * [Client] Add: peerjs voice component
    
    * [Client] Add: Peerjs users call
    
    * [Client] Add: Peerjs users call
    
    * [client] Fix: ui
    
    * [client] Fix: ui
    
    * [client] Fix: ui
    
    * [client] Fix: ui
    
    * [client] Fix: ui
    
    * [client] Fix: ui
    
    * back
    
    * back
    
    * [Client] Fix: UI
    
    * [Client] Fix: peer.js
    
    * [Client] Fix: peer.js
    
    * [Client] Fix: UI
    
    * [Client] Fix: UI
    
    * [Client] Fix: UI

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 import NavBar from "../../components/NavBar/NavBar";
-import BookMarkList from "../../components/MypageDashBoardContent/BookMarkList/BookMarkList";
-import MessagesList from "../../components/MypageDashBoardContent/MessagesList/MessagesList";
 import SettingInfo from "../../components/MypageDashBoardContent/SettingInfo/SettingInfo";
 import { Cookies } from "react-cookie";
-
-import {
-  IoHomeOutline,
-  IoChatbubblesOutline,
-  IoSettingsOutline,
-} from "react-icons/io5";
 
 import "./MyPage.scss";
 
@@ -21,97 +13,26 @@ function MyPage() {
   const [profileImg, setProfileImg] = useState(null);
   const [tab, setTab] = useState(3);
 
-  const actionhandle = (index) => {
-    setTab(index);
-  };
   useEffect(() => {
     setProfileImg(userInfo.avatar);
   }, [userInfo]);
-
-  // const imagehandler = (e) => {
-  //   const reader = new FileReader();
-  //   reader.onload = () => {
-  //     if (reader.readyState === 2) {
-  //       setProfileImg(reader.result);
-  //     }
-  //   };
-  //   reader.readAsDataURL(e.target.files[0]);
-  // };
 
   return (
     <div className="mypage__container">
       <div className="mypage__nav">
         <NavBar />
       </div>
-      <div className="mypage__main">
-        <div className="mypage__dashboard">
-          <div className="mypage__dashboard-tabs">
-            <div className="mypage__user">
-              <img
-                className="mypage__img"
-                src={
-                  avatar === null
-                    ? require("../../images/dummy/white.jpeg").default
-                    : avatar
-                }
-                alt=""
-              />
-              <h2 className="mypage__h2">{nickname}</h2>
-              <h3 className="mypage__h3">{status}</h3>
-            </div>
-            <div className="mypage__tabs">
-              {/* <div
-                onClick={() => actionhandle(1)}
-                className={tab === 1 ? "mypage__tab active-tab" : "mypage__tab"}
-              >
-                <div className="mypage__tab-icon">
-                  <IoHomeOutline size={25} />
-                </div>
-                <span className="mypage__tab-title">SERVERS</span>
-              </div>
-              <div
-                onClick={() => actionhandle(2)}
-                className={tab === 2 ? "mypage__tab active-tab" : "mypage__tab"}
-              >
-                <div className="mypage__tab-icon">
-                  <IoChatbubblesOutline size={25} />
-                </div>
-                <span className="mypage__tab-title">FRIENDS</span>
-              </div> */}
-              <div
-                onClick={() => actionhandle(3)}
-                className={tab === 3 ? "mypage__tab active-tab" : "mypage__tab"}
-              >
-                <div className="mypage__tab-icon">
-                  <IoSettingsOutline size={25} />
-                </div>
-                <span className="mypage__tab-title">SETTING</span>
-              </div>
-            </div>
-          </div>
-          <div className="mypage__dashboard-content">
-            <div
-              className={
-                tab === 1 ? "mypage__content active-content" : "mypage__content"
-              }
-            >
-              <BookMarkList />
-            </div>
-            <div
-              className={
-                tab === 2 ? "mypage__content active-content" : "mypage__content"
-              }
-            >
-              <MessagesList />
-            </div>
-            <div
-              className={
-                tab === 3 ? "mypage__content active-content" : "mypage__content"
-              }
-            >
-              <SettingInfo avatar={avatar} status={status} oauth={oauth} />
-            </div>
-          </div>
+      <div className="mypage__profile">
+        <div className="mypage__user">
+          <img
+            className="mypage__img"
+            src={
+              avatar === null
+                ? require("../../images/dummy/white.jpeg").default
+                : avatar
+            }
+            alt=""
+          />
         </div>
       </div>
     </div>
