8551cf8d21d1c3093c06455bd314b537f2b43641
xinnni <79839230+xinnni@users.noreply.github.com>
2021-10-07 16:25:41 +0900

    [Client] Fix: socket.io

--- src/pages/ChatPage/ChatPage.js
+++ src/pages/ChatPage/ChatPage.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+
+import useSocket from "../../hooks/useSocket";
+import { Cookies } from "react-cookie";
 
 import NavBar from "../../components/NavBar/NavBar";
 import SideBar from "../../UI/SideBar/SideBar";
@@ -9,8 +12,60 @@ import Voice from "../../components/Chat/Voice/Voice";
 import "./ChatPage.scss";
 
 const ChatPage = (props) => {
-  const path = useParams();
+  const cookies = new Cookies();
+  let userInfo = cookies.get("userInfo");
+  const history = useHistory();
+
+  const [newMsg, setNewMsg] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [currentUser, setCurrentUser] = useState();
+
+  const { gameId, roomId, chatId } = useParams();
+
+  const { id, userId, avatar, nickname } = userInfo;
+  userInfo = { id, userId, avatar, nickname };
+
+  const { socket, joinRoom } = useSocket(gameId, roomId, userInfo);
+  useEffect(() => {
+    joinRoom();
+
+    socket.current.on("roomMessage", (userData, msgData) => {
+      const incomingMsg = {
+        body: msgData,
+        user: userData.userId,
+        mine: userData.userId === userInfo.userId,
+      };
+      setMessages((prevM) => [...prevM, incomingMsg]);
+    });
+
+    socket.current.on("welcomeRoom", (data) => {
+      const len = data.length;
+      setCurrentUser(len);
+    });
+    socket.current.on("fullRoom", () => {
+      history.goBack(-1);
+    });
+
+    return () => {
+      socket.current.off("welcomeRoom");
+      socket.current.off("roomMessage");
+      socket.current.off("fullRoom");
+      socket.current.disconnect();
+    };
+  }, [gameId, roomId]);
+
+  const msgInputHandler = (e) => {
+    setNewMsg(e.target.value);
+    e.preventDefault();
+  };
+  const sendHandler = (e) => {
+    socket.current.emit("roomMessage", roomId, chatId, userInfo, newMsg);
+    e.preventDefault();
+    setNewMsg("");
+  };
+
   const { roomTitle } = props.location.state;
+
   return (
     <div className="chatpage-container">
       <div className="chatpage-nav">
@@ -19,11 +74,16 @@ const ChatPage = (props) => {
       <div className="chatpage-main">
         <div className="chatpage-main-container">
           <div className="chatpage-main-header">
-            <ChatHeader path={path} roomTitle={roomTitle} />
+            <ChatHeader roomTitle={roomTitle} currentUser={currentUser} />
           </div>
           <div className="chatpage-main-content">
             <div className="chatpage-chat">
-              <Chat />
+              <Chat
+                messages={messages}
+                msgInputHandler={msgInputHandler}
+                newMsg={newMsg}
+                sendHandler={sendHandler}
+              />
             </div>
             <div className="chatpage-vioce">
               <Voice />
